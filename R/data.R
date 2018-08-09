ad566c463a56f3d0e3e85a4e26715ccad738d8e5
Robin Lovelace <rob00x@gmail.com>
2018-08-09 16:41:47 +0100

    Add ics file

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Example .ics file on English and Welsh holidays
+#'
+#' This file is documented at: [gov.uk/government/publications/open-standards-for-government/exchange-of-calendar-events](https://www.gov.uk/government/publications/open-standards-for-government/exchange-of-calendar-events)
+#'
+#' @examples
+#' # dataset was stored as follows:
+#' # download.file("https://www.gov.uk/bank-holidays/england-and-wales.ics", "inst/extdata/england-and-wales.ics")
+#' # ics_read(system.file("extdata", "england-and-wales.ics", package = "ical"))
+#' # holidays = "holiday"
+#' # usethis::use_data(holidays)
+"holidays"
