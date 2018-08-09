f2bdf2dee3b31aea2fe261498ebcc3e15deb45df
layik <layik@users-mbp.wireless.leeds.ac.uk>
2018-08-09 17:03:50 +0100

    Skeleton ic_read

--- R/ic_read.R
+++ R/ic_read.R
@@ -1,3 +1,15 @@
 #' Read ics file
 #'
+#' @param file ics file to read
+#' @return object of class ics
+#' @export
 #'
+#' @examples
+#' f <- system.file("extdata", "england-and-wales.ics", package = "ical")
+#' ic_read(f)
+ic_read <- function(file) {
+  # TODO: type check & stop
+  contents <- readLines(file)
+  # do something with the contents
+  contents[1]
+}
