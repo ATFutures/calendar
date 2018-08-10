de1b70a3a9060d35e7c9c8edf66597f9127ea07a
Robin Lovelace <rob00x@gmail.com>
2018-08-10 10:45:06 +0100

    Add ic_example minial dataset - cc @layik

--- R/data.R
+++ R/data.R
@@ -10,15 +10,13 @@
 #' # holidays = "holidays"
 #' # usethis::use_data(holidays)
 "holidays"
-#' Example .ics file on English and Welsh holidays
+
+#' Minimal example of raw ical file (already read-in from file)
 #'
-#' This file is documented at: [gov.uk/government/publications/open-standards-for-government/exchange-of-calendar-events](https://www.gov.uk/government/publications/open-standards-for-government/exchange-of-calendar-events)
+#' See [here](https://calendar.google.com/calendar/ical/9sl1qu3qh2vdnq26bjgvtnos94%40group.calendar.google.com/private-85cbe5d781da1b7efc91e01032cfc264/basic.ics) for the file.
 #'
 #' @examples
-#' # dataset was stored as follows:
-#' # download.file("https://www.gov.uk/bank-holidays/england-and-wales.ics",
-#' # "inst/extdata/england-and-wales.ics")
-#' # ics_read(system.file("extdata", "england-and-wales.ics", package = "ical"))
-#' # holidays = "holidays"
-#' # usethis::use_data(holidays)
-"holidays"
+#' # download.file("long_url...", "inst/extdata/example.ics")
+#' ic_example = readLines(system.file("extdata", "example.ics", package = "ical"))
+#' # usethis::use_data(ic_example)
+"ic_example"
--- R/ic_read.R
+++ R/ic_read.R
@@ -12,13 +12,8 @@ ic_read <- function(file) {
 
   x <- readLines(file)
 
-  # from https://stackoverflow.com/questions/43573982/how-to-import-ical-ics-file-in-r
-  stopifnot(!any(grepl("^\\s+", x)))
-  keyval <- do.call(rbind, regmatches(x, regexpr(":", x, fixed = TRUE), invert = TRUE))
-  keyval <- keyval[which.max(keyval[,1]=="BEGIN" & keyval[,2]=="VEVENT"):utils::tail(which(keyval[,1]=="END" & keyval[,2]=="VEVENT"), 1),]
-  keyval <- cbind.data.frame(keyval, id=cumsum(keyval[,1]=="BEGIN" & keyval[,2]=="VEVENT"))
-  df <- stats::reshape(keyval, timevar="1", idvar="id", direction = "wide")
-  # head(df[,c(3,4,9)])
+  # previous approach: see https://stackoverflow.com/questions/43573982/how-to-import-ical-ics-file-in-r
+  timestamps_char = ic_find(x, "DTSTAMP")
 
   # do something with the contents
   df
