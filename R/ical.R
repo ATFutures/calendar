67fbe4e60fed751ead3be7b756d3cadfba39bd9e
Robin Lovelace <rob00x@gmail.com>
2018-09-23 20:41:13 +0100

    Add round trip function ic_character

--- R/ical.R
+++ R/ical.R
@@ -20,6 +20,32 @@ ical <- function(x) {
 #' ic_attributes_vec(ical_example)
 ic_attributes_vec <- function(x) {
   line_first_event <- grep("BEGIN:VEVENT", x)[1]
-  x_attributes <- x[1:line_first_event]
-  ic_attr_vec <- ic_vector(x_attributes, pattern = "*")
+  x_attributes <- x[1:(line_first_event - 1)]
+  ic_vector(x_attributes, pattern = "*")
+}
+#' Convert ical object to character strings of attributes
+#'
+#' @param ic object of class `ical`
+#' @export
+#' @examples
+#' ic <- ical(ical_example)
+#' ic_character(ic)
+#' ical_example == ic_character(ic)
+ic_character <- function(ic) {
+  char_attributes <- paste(names(attributes(ic)$ical), attributes(ic)$ical, sep = ":")
+  char_events <- ic_char_event(ic)
+  c(char_attributes, char_events, "END:VCALENDAR")
+}
+#' Convert ical object to character strings of events
+#'
+#' @param ic object of class `ical`
+#' @export
+#' @examples
+#' ic <- ical(ical_example)
+#' ic_char_event(ic)
+#' ic_char_event(ic[c(1, 1), ])
+ic_char_event <- function(ic) {
+  char_names <- c(rep(c("BEGIN", names(ic), "END"), nrow(ic)))
+  char_contents <-  apply(ic, 1, function(x) c("VEVENT", as.character(x), "VEVENT"))
+  paste(char_names, char_contents, sep = ":")
 }
