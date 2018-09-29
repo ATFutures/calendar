232908877faffa50a2c7f6fee6d099cab402366e
Layik <layik.hama@gmail.com>
2018-09-29 09:55:13 +0100

    Merge ic_event and _raw to take different types of vars, test cov in #17

--- tests/testthat/test-test-ic_event.R
+++ tests/testthat/test-test-ic_event.R
@@ -1,22 +1,21 @@
 context("test-test-ic_event")
 
 test_that("error if start or end is NA", {
-  expect_error(ic_event(start_time = NA, end_time = NA))
+  expect_warning(ic_event(start_time = NA, end_time = NA))
 })
 
 test_that("error if start or end is empty string", {
   expect_error(ic_event(start_time = Sys.time(), end_time = ""))
 })
 
-test_that("pass", {
+test_that("default values work", {
   expect_equal(class(ic_event()), c("ical", "tbl_df", "tbl", "data.frame"))
 })
 
-context("test-test-ic_event_raw")
-test_that("pass", {
-  expect_equal(class(ic_event_raw()), c("ical", "tbl_df", "tbl", "data.frame"))
+test_that("2018-10-12 15:00, end_time = 2018-10-13 15:00 matches default format", {
+  expect_equal(class(ic_event()), c("ical", "tbl_df", "tbl", "data.frame"))
 })
 
 test_that("error if start or end is empty string", {
-  expect_warning(ic_event_raw(start_time = Sys.time(), end_time = ""))
+  expect_error(ic_event(start_time = Sys.time(), end_time = ""))
 })
