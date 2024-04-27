30b5b4b9706f2b6c5fab043fa7025db8e4cfd76f
Ollie Lloyd <o.lloyd@doctors.org.uk>
2024-04-27 15:51:54 +0100

    updated test-ic-extract to set tz=Europe/London for both the test and comparator datetime

--- tests/testthat/test-ic-extract.R
+++ tests/testthat/test-ic-extract.R
@@ -1,6 +1,12 @@
 context("test-ic-extract")
 
+# now that we are setting zulu time strings to zulu time in ic_date() to test against a specific 
+# time zone datetime we need to set it first
 test_that("ic_extract works", {
-  expect_equal(ic_extract(ical_example, "DTSTART"),
-               as.POSIXct("2018-08-09 16:00:00 BST"))
+  test_datetime <- ic_extract(ical_example, "DTSTART")
+  attr(test_datetime, "tzone") <- "Europe/London"
+  expect_equal(test_datetime,
+               as.POSIXct("2018-08-09 17:00:00", tz = "Europe/London"))
 })
+
+
